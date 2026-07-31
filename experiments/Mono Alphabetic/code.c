0823768fac0e544cbb2e88417b6e812d8a64eccb
192565085simats-debug <192565085.simats@saveetha.com>
2026-07-31 14:13:13 +0530

    Update code.c

--- experiments/Mono Alphabetic/code.c
+++ experiments/Mono Alphabetic/code.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char plaintext[100], ciphertext[100];
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char substitute[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    int i, j;
+
+    printf("Enter plaintext: ");
+    fgets(plaintext, sizeof(plaintext), stdin);
+
+    for (i = 0; plaintext[i] != '\0'; i++) {
+        if (plaintext[i] >= 'A' && plaintext[i] <= 'Z') {
+            for (j = 0; j < 26; j++) {
+                if (plaintext[i] == alphabet[j]) {
+                    ciphertext[i] = substitute[j];
+                    break;
+                }
+            }
+        }
+        else if (plaintext[i] >= 'a' && plaintext[i] <= 'z') {
+            for (j = 0; j < 26; j++) {
+                if (plaintext[i] == alphabet[j] + 32) {
+                    ciphertext[i] = substitute[j] + 32;
+                    break;
+                }
+            }
+        }
+        else {
+            ciphertext[i] = plaintext[i];
+        }
+    }
+
+    ciphertext[i] = '\0';
+
+    printf("Encrypted message: %s", ciphertext);
+
+    return 0;
+}
+
 
